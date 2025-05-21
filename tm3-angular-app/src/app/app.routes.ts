c35a594482dbe456a0930ae3083e9e669c28bb5a
Ella Nikola Barkane <eb23122@edu.lu.lv>
2025-05-21 19:49:13 +0300

    TM-10 Setup routing for task list application

--- tm3-angular-app/src/app/app.routes.ts
+++ tm3-angular-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TaskListPageComponent } from './pages/task-list-page/task-list-page.component';
+import { CreateTaskPageComponent } from './pages/create-task-page/create-task-page.component';
+import { TaskDetailsPageComponent } from './pages/task-details-page/task-details-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TaskListPageComponent },
+  { path: 'create', component: CreateTaskPageComponent },
+  { path: 'details/:id', component: TaskDetailsPageComponent }
+];
